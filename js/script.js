f49c604971cfd90fd3b530db646afa79be2f402c
Ubuntu <ubuntu@ip-172-31-86-59.ec2.internal>
2019-09-05 17:19:19 +0000

    change table colours

---
+++ js/script.js
@@ -0,0 +1,65 @@
+//waits until page is ready
+
+$(document).ready(function(){
+//will need to comment out some code when trying to view effects on theeir own
+ //hides all panels when a panel is clicked
+
+$(".theButton").click(function(){
+  $("#panel .container").siblings().hide();
+
+   });
+
+//hides only the panel that was clicked
+  $(".theButton").click(function(){
+  $(this).hide();
+
+   });
+
+//adds  a fadeTo to all panels when a panel is clicked
+$(".theButton").click(function(){
+	$  ("#panel .container").siblings().fadeTo(1000, .5);
+
+   });
+
+
+//restores all panels to full opacity when reset button clicked 
+
+$(".superButton").click(function(){
+	$("#panel .container").siblings().fadeTo(1000,1);	
+
+   });
+
+//turns panel background black on mouseenter
+
+$(".theButton").mouseenter(function(){
+	$(this).addClass("makeBlack");
+
+   });
+   
+ //returns to original colour on mouseout 
+
+$(".theButton").mouseout(function(){
+	$(this).removeClass("makeBlack");
+
+   });
+   
+   //will wait uuntil page ready
+
+ $(document).ready(function(){
+
+    /*when table header is clicked , removes selection class from all other table rows and
+
+      adds the selection class to this table headers row only*/
+
+     $("th").click(function(){
+
+     	$("tr").children().removeClass("selection")
+
+    	$(this).siblings().addClass("selection");
+
+  });
+
+ });
+
+});
+
